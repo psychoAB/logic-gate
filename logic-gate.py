0983218dfbe7b5386e941c97a2f862e15698b4b5
psychoAB <dome.2499@hotmail.com>
2016-12-22 18:00:59 +0700

    can create window

--- logic-gate.py
+++ logic-gate.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 
 import arcade
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+class LogicGateGameWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        arcade.start_render()
+
+if __name__ == '__main__':
+    window = LogicGateGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.run()
