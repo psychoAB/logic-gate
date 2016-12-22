b2df1f531a93bfa2a46ff94c4bb45772e3d4b4d5
psychoAB <dome.2499@hotmail.com>
2016-12-22 23:17:26 +0700

    can create gate in game

--- logic-gate.py
+++ logic-gate.py
@@ -5,36 +5,66 @@ import arcade
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
+class SpriteModel(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+
+        self.set_position(kwargs['x'], kwargs['y'])
+
+        sprite_texture = self.textures[0]
+
+        self.update()
+
+    def update(self):
+        super().update()
+
+        sprite_texture = self.textures[0]
+
+        self.texture_left_margin = self.center_x - sprite_texture.width / 2
+        self.texture_right_margin = self.center_x + sprite_texture.width / 2
+        self.texture_down_margin = self.center_y - sprite_texture.height / 2
+        self.texture_up_margin = self.center_y + sprite_texture.height / 2
+        
+    def is_mouse_on(self, x, y):
+        self.update()
+        return x >= self.texture_left_margin and x <= self.texture_right_margin and y >= self.texture_down_margin and y <= self.texture_up_margin
+
 class LogicGateGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
         arcade.set_background_color(arcade.color.WHITE) 
 
-        self.or_gate_sprite = arcade.Sprite('images/or_gate.png')
-        self.or_gate_sprite.set_position(100, 100)
+        self.main_or_gate = SpriteModel('images/or_gate.png', x = 100, y = 100)
 
+        self.or_gate_sprites = []
         self.dragging = False
 
     def on_draw(self):
         arcade.start_render()
 
-        self.or_gate_sprite.draw()
+        self.main_or_gate.draw()
+
+        for or_gate_sprite in self.or_gate_sprites:
+            or_gate_sprite.draw()
 
     def on_mouse_motion(self, x, y, dx, dy):
-        print(str(x) + " " + str(y))
+        #print(str(x) + " " + str(y))
+        pass
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         if self.dragging:
-            self.or_gate_sprite.set_position(x, y)
+            for or_gate_sprite in self.or_gate_sprites:
+                if or_gate_sprite.is_mouse_on(x, y):
+                    or_gate_sprite.set_position(x, y)
 
     def on_mouse_press(self, x, y, button, modifiers):
-        or_gate_texture = self.or_gate_sprite.textures[0]
-
         if button == arcade.MOUSE_BUTTON_LEFT:
-            if x >= self.or_gate_sprite.center_x - or_gate_texture.width / 2 and x <= self.or_gate_sprite.center_x + or_gate_texture.width / 2:
-                if y >= self.or_gate_sprite.center_y - or_gate_texture.height / 2 and y <= self.or_gate_sprite.center_y + or_gate_texture.height / 2:
-                    self.dragging = True
+            self.dragging = True
+
+            if self.main_or_gate.is_mouse_on(x, y):
+                new_or_gate = SpriteModel('images/or_gate.png', x = x , y = y)
+                self.or_gate_sprites.append(new_or_gate)
 
     def on_mouse_release(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
