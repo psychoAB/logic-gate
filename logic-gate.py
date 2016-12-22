c7a945dc90f3ef54b057efbcf8b528ad24753244
psychoAB <dome.2499@hotmail.com>
2016-12-22 18:32:56 +0700

    can draw sprite

--- logic-gate.py
+++ logic-gate.py
@@ -9,10 +9,15 @@ class LogicGateGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.BLACK)
+        arcade.set_background_color(arcade.color.WHITE) 
+
+        self.orGate = arcade.Sprite('images/or_gate.png')
+        self.orGate.set_position(100, 100)
 
     def on_draw(self):
         arcade.start_render()
+
+        self.orGate.draw()
 
 if __name__ == '__main__':
     window = LogicGateGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
